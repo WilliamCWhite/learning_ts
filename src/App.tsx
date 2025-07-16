0b50f0f772fb04d7fbb6cb38de6214e8ef3b313d
WilliamCWhite <williamcwhite323@gmail.com>
2025-07-15 22:49:46 -0400

    Basic Google OAuth functionality and a test to ensure JWT token usage implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import Header from './components/Header.tsx'
 import Entry from './components/Entry.tsx'
+import { GoogleLogin } from '@react-oauth/google';
+import type { CredentialResponse } from '@react-oauth/google';
+import { useState } from 'react';
 
 import "./styles/App.css";
 
@@ -10,6 +13,8 @@ function App() {
     name: string;
     score: number;
   }
+
+  const [jwtToken, setJwtToken] = useState("")
 
   const testList : Entry[] = data.lists[0].entries;
 
@@ -23,6 +28,18 @@ function App() {
     )
   })
 
+  function testRequest() :void {
+    console.log("Initializing testRequest")
+    fetch('http://localhost:7070/api/tokentest', {
+      method: 'GET',
+      headers: {'Authorization': `Bearer ${jwtToken}`}
+    }).then(res => res.json())
+    .then(data => {
+      console.log("Printing data from testRequest")
+      console.log(data)
+    })
+  }
+
   return (
     <div className="w-screen h-screen flex justify-center items-center bg-white">
       <main className="w-5xl h-full my-16 bg-gray-100">
@@ -30,6 +47,26 @@ function App() {
         <section className="flex flex-col w-full">
           {componentList}
         </section>
+        <GoogleLogin
+          onSuccess={(credentialResponse : CredentialResponse) => {
+            console.log(credentialResponse)
+            const idToken = credentialResponse.credential;
+            fetch('http://localhost:7070/auth/google', {
+              method: 'POST',
+              headers: { 'Content-Type': 'application/json' },
+              body: JSON.stringify({ "id_token": idToken })
+            }).then(res => res.json())
+            .then(data => {
+                console.log("Writing post request response below")
+                console.log(data)
+                setJwtToken(data.token)
+            });
+          }}
+          onError={() => {
+            console.log("Login Failed")
+          }}
+        />
+        <button onClick={testRequest} className="w-10 h-10 border-2"></button>
       </main>
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import './styles/index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="934499925133-rfv5i2cqavmutiedelnh1o7tu6l1lvdj.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
