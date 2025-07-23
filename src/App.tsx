00d1c0f73a10b79f558928ca951a9fea35cb8c91
WilliamCWhite <williamcwhite323@gmail.com>
2025-07-23 19:40:54 -0400

    Logging users in and out with JWT's and storing them in cookies fully functional

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,67 @@
-import { Routes, Route, useNavigate } from 'react-router-dom'
-import About from './pages/About.tsx'
-import Login from './pages/Login.tsx'
-import { useEffect, useState } from 'react';
-import ProtectedRoute from './components/ProtectedRoute.tsx';
+import { Routes, Route, useNavigate } from "react-router-dom";
+import About from "./pages/About.tsx";
+import Login from "./pages/Login.tsx";
+import { useEffect, useState } from "react";
+import TestDashboard from "./pages/TestDashboard.tsx";
+import Cookies from "js-cookie"
 
 function App() {
-  const [jwtToken, setJwtToken] = useState("")
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [jwtToken, setJwtToken] = useState<string>("");
+  const navigate = useNavigate()
 
   useEffect(() => {
-    if (jwtToken.length === 0) {
-      setIsLoggedIn(false)
+    const cookieJwt = Cookies.get("jwtToken")
+    if (cookieJwt !== undefined && cookieJwt !== "") {
+      setJwtToken(cookieJwt)
+      navigate("/dashboard")
     }
     else {
-      setIsLoggedIn(true)
+      navigate("/login")
     }
-  }, [jwtToken, isLoggedIn])
+  }, [])
+
+  function handleJwtFailure(
+    statusCode: number,
+    navigate: (path: string) => void,
+  ) {
+    if (statusCode !== 401 && statusCode !== 403) {
+      return;
+    }
+
+    setJwtToken("");
+    Cookies.set("jwtToken", "")
+    navigate("/login");
+    throw new Error("unauthorized");
+  }
+
+  function SignOut(navigate: (path: string) => void) {
+    setJwtToken("")
+    Cookies.set("jwtToken", "")
+    navigate("/login")
+  }
 
   return (
     <>
       <Routes>
-        <Route path="/login" element={<Login jwtToken={jwtToken} setJwtToken={setJwtToken}/>} />
-
-        <Route element={<ProtectedRoute isAuthenticated={isLoggedIn} />}>
-          <Route path="/about" element={<About />} />
-        </Route>
+        <Route
+          path="/login"
+          element={<Login jwtToken={jwtToken} setJwtToken={setJwtToken} />}
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <TestDashboard
+              jwtToken={jwtToken}
+              setJwtToken={setJwtToken}
+              handleJwtFailure={handleJwtFailure}
+              SignOut={SignOut}
+            />
+          }
+        />
+        <Route path="/about" element={<About />} />
       </Routes>
     </>
-  )
+  );
 }
 
 export default App;
